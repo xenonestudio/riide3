ea764cbdcd64f685227b3a293a764f7029225c72
bahimer <bahimer8080@gmail.com>
2021-03-02 09:43:43 -0400

    headers cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -21,6 +21,6 @@ class Cors
       //Métodos que a los que se da acceso
       ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
       //Headers de la petición
-      ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+      ->header("Access-Control-Allow-Headers", "*");
     }
 }
