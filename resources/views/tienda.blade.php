cd2f9297cd12851b530963437791df706ac1339e
bahimer <bahimer8080@gmail.com>
2021-03-10 10:17:29 -0400

    lupa tiendas

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -73,7 +73,11 @@
     <div class="row pt-2">
         <div class="col-12 py-3">
             <div class="form-group has-search px-3 m-0 " style="border-radius: 10px ;">
-                <span class="fas fa-search form-control-feedback"></span>
+                <span class="form-control-feedback">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </span>
                 <input id="search-store" type="text" class="form-control m-0" placeholder="Search" style="border-radius: 10px ;background: #bdbdbd  ;">
             </div>
         </div>
