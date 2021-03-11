41286c627e9c4d890146d2bff58655002a1c71cf
bahimer <bahimer8080@gmail.com>
2021-03-11 15:18:11 -0400

    modificacion

--- app/CarteleraPancarta.php
+++ app/CarteleraPancarta.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CarteleraPancarta extends Model
 {
     protected $table = 'cartelera_pancarta';   
+
+    public function pancartas()
+    {
+        return $this->belongsTo('App\Pancarta');
+    }
 }
