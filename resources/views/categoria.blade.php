fb8aa16ec4cc951cc78a3226f2b3b92aeb8c8788
bahimer <bahimer8080@gmail.com>
2021-03-23 11:02:48 -0400

    text dark cats

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -2,7 +2,7 @@
 <a href="/categorias/{{$categoria["id"]}}" class="card w-100 mt-2 p-2 href-categorias" style="text-decoration: none">
     <div class="w-100 card-img-cat" style="background-image: url('/storage/{{ $categoria["imagen"] }}') ;background-position: center;background-size: cover;"></div>
     <div class="card-body p-0">
-      <p class="card-text text-center size-title-card pt-2 text-muted">
+      <p class="card-text text-center size-title-card pt-2 text-dark">
           {{ $categoria["categoria"] }}
       </p>
     </div>
